3f7f7e31cd5660bcaf7b5823d810a33b70601738
basilesimon <basile@basilesimon.fr>
2017-11-16 17:17:45 +0000

    Get data from form and subsequently display dropdown of cases

--- app/budget-calculator/script.js
+++ app/budget-calculator/script.js
@@ -19,17 +19,69 @@ if (typeof document !== 'undefined') {
     // Called after the element is attached to the document
     attached: function() {
       this.form = this.$['form'];
-      this.form.addEventListener('submit', this.doAThing.bind(this), false);
+      this.form.addEventListener(
+        'submit',
+        this.getDataFromUser.bind(this),
+        false
+      );
     },
 
     // Called after the element is detached from the document
     detached: function() {
-      this.button.removeEventListener('click', this.doAThing.bind(this), false);
+      this.button.removeEventListener(
+        'click',
+        this.getDataFromUser.bind(this),
+        false
+      );
     },
 
-    doAThing: function(e) {
+    getDataFromUser: function(e) {
       e.preventDefault();
-      console.log(e);
+      this.$$('.error').innerHTML = '';
+
+      // income from text box
+      const income = this.$['incomeInput'].value.replace(/,/g, '');
+      const cleanIncome = parseInt(income);
+      if (isNaN(cleanIncome)) {
+        this.$$('.error').innerHTML =
+          'THERE HAS BEEN A PROBLEM PARSING YOUR INCOME.';
+        return;
+      }
+
+      // household type: single or couple from radio checkboxes
+      let household = null;
+      const checkBoxesFormData = new FormData(this.form);
+      for (const entry of checkBoxesFormData) {
+        household = entry[1];
+      }
+
+      // dropdown: will include the discrete list of cases from the dataset
+      if (this.dropdownVisible === true) {
+        const dropdownSelection = document.getElementById('dropdown').options[
+          document.getElementById('dropdown').selectedIndex
+        ].value;
+        this.data.dropdownSelection = dropdownSelection;
+
+        // at this stage, the dropdown has been displayed
+        // and the reader has made a choice.
+        // we've got enough to display our things
+        this.showData(this.data);
+      }
+
+      this.data = {
+        cleanIncome,
+        household,
+      };
+      this.displayDropdown();
+    },
+
+    displayDropdown: function() {
+      document.getElementById('dropdown').classList.add('visible');
+      this.dropdownVisible = true;
+    },
+
+    showData: function(data) {
+      console.log(data);
     },
   });
 }
